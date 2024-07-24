2f040a4f7984edd75a31bf41d1ead87a8e199b2e
Senin Maksim <wecpo@yandex.ru>
2024-07-24 19:50:56 +0300

    Присвоил значение для timeoutid (для вывода типа), закостылил отписку от событий.

--- debounceScript.js
+++ debounceScript.js
@@ -1,13 +1,22 @@
 const debounceInput = document.querySelector("#debounceInput");
 const deboncedValueSpan = document.querySelector("#debouncedValue");
 
-let timeoutId;
+let timeoutId = 0;
 
-const debounceFunc = (value, delay) => {
+const debounceFunc = (event) => {
+  const value = event.target.value;
+
   clearTimeout(timeoutId);
-  timeoutId = setTimeout(() => (deboncedValueSpan.textContent = value), delay);
+
+  timeoutId = setTimeout(() => (deboncedValueSpan.textContent = value), 1000);
 };
 
-debounceInput.addEventListener("input", (event) =>
-  debounceFunc(event.target.value, 1000)
+debounceInput.addEventListener("input", debounceFunc);
+
+debounceInput.addEventListener(
+  "blur",
+  () => debounceInput.removeEventListener("input", debounceFunc),
+  {
+    once: true,
+  }
 );
